c4d7ea70fb4542cdea43574f59505c601ad4e63e
KrishnaPayyavula <payyavulavenkatakrishna@outlook.com>
2019-07-24 15:45:23 +0530

    i have added extra lines in the sample.py file

---
+++ sample.py
@@ -0,0 +1,14 @@
+import html_parser
+import xml_parser
+
+import UI_parser
+
+def a:
+   print("This is sample text in the sample.py file")
+   Print("This is second time I am adding files to the sample.y file")
+
+
+a()
+
+if __main__=__init__:
+	a()
